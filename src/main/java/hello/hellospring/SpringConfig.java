aa683014fc3924185839e23ef1c14bfc51db1ddf
sungwoo <asw4ever@naver.com>
2024-01-18 21:14:23 +0900

    sql 생성

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
+    //자바 코드로 직접 스프링빈을 등록할 때
 //    @Bean
 //    public MemberService memberService(){
 //        return new MemberService(memberRepository());
