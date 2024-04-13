e398bfe9ffde244ed60800672162cab108996c1b
ansungwoo <asw4ever@naver.com>
2024-04-13 18:02:12 +0900

    member 리스트 기능 추가

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -6,19 +6,25 @@ import com.example.firstproject.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import java.util.List;
+
 @Slf4j
 @Controller
 public class MemberController {
     @Autowired
     private MemberRepository memberRepository;
-    @GetMapping("/members/new")
-    public String NewMemberForm(){
+    @GetMapping("/signup")
+    public String SignUpPage(){
         return "members/new";
     }
-    @PostMapping ("members/create")
-    public String createMember(MemberForm form){
+    @PostMapping ("/join")
+    public String join(MemberForm form){
+        log.info(form.toString());
+
         Member member = form.toEntity(); //DTO를 entity로 변환
         //System.out.println(member.toString());
         log.info(member.toString());
@@ -28,5 +34,25 @@ public class MemberController {
         //System.out.println(saved.toString());
 
         return "";
+    }
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model){
+        log.info("id = " + id );
+        // 1. id를 조회해 데이터 가져오기
+        Member memberentity = memberRepository.findById(id).orElse(null);
+        // id가 없으면 null을 반환한다.
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("member",memberentity);
+        // 3. 뷰 페이지 반환하기
+        return "members/show";
+    }
+    @GetMapping("/members")
+    public String index(Model model){
+        // 1 . 모든 데이터 가져오기
+        List<Member> memberEntityList = memberRepository.findAll();
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("memberList",memberEntityList);
+        // 3. 뷰 페이지 설정하기
+        return "members/index";
     }
 }
