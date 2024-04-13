7a51bb00244d57f9c91e847cd52846284dd5e29a
ansungwoo <asw4ever@naver.com>
2024-04-13 15:59:53 +0900

    refact: entity, dto 리팩토링

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,25 +1,14 @@
 package com.example.firstproject.dto;
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class ArticleForm {
     private Long id;
     private String title;
     private String content;
-
-    //생성자 추가
-    public ArticleForm(Long id,String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-    //데이터 확인용 코드
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
         return new Article(null,title,content);
--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -4,9 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Article {
     @Id
     @GeneratedValue
@@ -15,21 +20,6 @@ public class Article {
     private String title;
     @Column
     private String content;
-
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
 
 
